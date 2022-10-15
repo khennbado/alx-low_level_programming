cfd8561d9b8e194b0d565a34d22d3356e233d8ab
khennbado <khennybado@gmail.com>
2022-10-15 16:25:17 -0700

    base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: print base16 lowercase numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+	char a;
+
+	n = 0;
+	a = 'a';
+
+	while
+		(n < 10) {
+			putchar(n + '0');
+			n++;
+		}
+	while
+		(a <= 'f') {
+			putchar(a);
+			a++;
+		}
+	putchar('\n');
+	return (0);
+}
