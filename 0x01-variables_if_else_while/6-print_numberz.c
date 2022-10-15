c68ee832f904692bab43dc7fd13960cfa9bda61d
khennbado <khennybado@gmail.com>
2022-10-15 15:25:30 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: print numberz
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	n = 0;
+	while
+		(n <= 10) {
+			putchar(n);
+			n++;
+		}
+	putchar('\n');
+	return (0);
+}
