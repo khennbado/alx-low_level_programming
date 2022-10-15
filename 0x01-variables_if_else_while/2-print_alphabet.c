4f337d46eb251522cd7e7e9950848c7176d40246
khennbado <khennybado@gmail.com>
2022-10-15 11:40:00 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Desctiption: prints alphabet
+ * Return: 0
+ */
+int main(void)
+{
+	char s;
+
+	s = 'a';
+	while
+		(s <= 'z') {
+			putchar(s);
+			s++;
+		}
+	putchar('\n');
+	return (0);
+}
