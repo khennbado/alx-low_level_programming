95f943b6200c60ac4ab832374fd45963129b56cf
khennbado <khennybado@gmail.com>
2022-10-19 12:19:01 -0700

    to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_to_98 - print to 98
+ * @n: number to print
+ * Return: number
+ */
+void print_to_98(int n)
+{
+	for (n = 0; n <= 98; n++)
+	{
+		_putchar(n);
+		_putchar(',');
+		_putchar(' ');
+	}
+	_putchar('\n');
+	return (n);
+}
