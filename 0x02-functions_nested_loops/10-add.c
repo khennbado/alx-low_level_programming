629bd2ee6acff194d4ae73670621f78286e93acf
khennbado <khennybado@gmail.com>
2022-10-19 11:51:17 -0700

    add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,26 +1,11 @@
 #include "main.h"
 /**
  * add - adds two numbers
- * @int: numbers to add
+ * @i: number to add
+ * @j: number to add
  * Return: sum of two numbers
  */
-int add(int, int)
+int add(int i, int j);
 {
-	int sum;
-	int num1;
-	int num2;
-
-	for (num1 = 0; num1 <= 9; num1++)
-	{
-		for (num2 = 0; num2 <= 9; num2++)
-		{
-			sum = add(num1, num2);
-			_putchar(sum);
-		}
-	}
-	_putchar('\n');
-	return (sum);
+	return (a + b);
 }
-
-
-
