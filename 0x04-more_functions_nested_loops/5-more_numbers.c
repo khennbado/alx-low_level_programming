7e10e2bc803ac7fdf1687d4ba9eba5bbec52664c
khennbado <khennybado@gmail.com>
2022-10-21 12:07:00 -0700

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - rint 10times 0-14
+ * Return: 0
+ */
+void more_numbers(void)
+{
+	int n, c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+			if (n > 9)
+			{
+				_putchar((n / 10) + '0');
+			}
+			_putchar((n % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
